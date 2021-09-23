3344a5cc6ae378881a1b824eb8cf8be7549e2ba9
Divyajain44 <dj.divyajain44@gmail.com>
2021-09-23 19:04:38 +0530

    some more files

---
+++ testing/api/controllers/locations_controller.go
@@ -0,0 +1,18 @@
+package controllers
+
+import (
+	"testing/golang-testing/src/api/services"
+
+	"github.com/gin-gonic/gin"
+
+	"net/http"
+)
+
+func GetCountry(c *gin.Context) {
+	country, err := services.LocationsService.GetCountry(c.Param("country_id"))
+	if err != nil {
+		c.JSON(err.Status, err)
+		return
+	}
+	c.JSON(http.StatusOK, country)
+}
---
+++ testing/api/main.go
@@ -0,0 +1,7 @@
+package main
+
+import "testing/golang-testing/src/api/app"
+
+func main() {
+	app.StartApp()
+}
